3edc7e824e105a4ddb764a65d1c99699ab3dcd73
BWNC-code <bryn.wheeler@gmail.com>
2023-01-14 14:05:48 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right!");
+        incrementScore();
     } else {
         alert(`Oh no! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -70,12 +71,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").textContent = ++oldScore;
 }
 
+/**
+ * Gets the current number of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").textContent = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
